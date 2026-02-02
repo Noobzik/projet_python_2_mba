3c30a7b63590f3f4a04177396f4990caf7908913
Florence MATIP <liliane_ngoyamb@yahoo.fr>
2026-02-02 23:23:10 +0100

    Dernière version

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import pandas as pd
 def connexion_dataset():
     # Chargement du dataset 
-    DATASET_PATH = "C:/Users/DELL/Documents/workspace/projet_python_2_mba/app/data/transaction_data.csv"
+    DATASET_PATH = "C:/Users/marie/Documents/projet_python_2_mba/app/data/transaction_data.csv"
     df = pd.read_csv(DATASET_PATH)
     return df
---
+++ app/router/customer.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Query
+from typing import List
+# Importation du DataFrame et des fonctions depuis ton service
+from app.services.customers import *
+from app.config import connexion_dataset
+
+df=connexion_dataset()
+# On laisse le prefix vide ici pour respecter tes noms de routes complets
+router_customers = APIRouter(
+    tags=["Customers"]
+)
+
+@router_customers.get("/api/customers")
+def list_customers_route():
+    # Appelle la fonction de service au lieu de retourner "suis la"
+    return list_customers(df)
+
+@router_customers.get("/api/customers/top")
+def get_top_customers(n: int = Query(10)):
+    return top_customers(df, n=n)
+
